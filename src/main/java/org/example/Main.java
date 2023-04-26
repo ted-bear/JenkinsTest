32ce648700cb5b297910a3a5f45c22194a4e3ee2
Egorka <trololo221@bk.ru>
2023-04-26 20:53:55 +0300

    Add main method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Docker!");
+    }
     public static int[] twoSum(int[] nums, int target) {
 
         Map<Integer, Integer> numsMap = new HashMap<>();
