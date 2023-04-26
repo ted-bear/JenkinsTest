496ea9d873200d68ae04fb6b48225cca691e6c69
Egorka <trololo221@bk.ru>
2023-04-26 20:10:31 +0300

    Add twoSum method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> numsMap = new HashMap<>();
+        int res1 = -1;
+        int res2 = -1;
+        int subtraction;
+
+        for (int i = 0; i < nums.length; i++) {
+            subtraction = target - nums[i];
+            if (numsMap.containsKey(subtraction)) {
+                return new int[] {numsMap.get(subtraction), i};
+            }
+            numsMap.put(nums[i], i);
+        }
+
+        return null;
     }
 }
